f2bfbbbb345b7a20611a9948368f023472612da1
lizolv <liz.olv@impactosdigitales.com>
2024-12-04 14:59:06 -0600

    10.4 try of repair controller auth

--- routes/Postulantes.js
+++ routes/Postulantes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../middlewares/authMiddleware');
-const  { getVacantesEmpresa, getPostulantes } = require('../controllers/postulanteController');
+const  { getVacantesEmpresa, getPostulantes } = require('../controllers/postulantesController');
 
 router.get('/api/VacanteEmpresa', verifyToken,  getVacantesEmpresa);
 router.get('/api/Postulantes', verifyToken, getPostulantes);
